e68723b732159ed71a38b2a523a0ddff1ae95056
Michael Scherr <mikescherr@gmail.com>
2016-03-09 11:28:01 -0500

    major refactor
    - switch to using postcss only
    - remove public/ folder for simplicity
    - add postcss config to separate file

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 (function(){
 	'use strict'; //eslint-disable-line
+
+	var App = {
+		app: document.querySelectorAll('[data-role="app"]')[0],
+		legend: document.querySelectorAll('[data-role="legend"]')[0],
+		renderLegend() {
+			console.log(this);
+		},
+		bindEvents() {
+			let appInputs = this.app.querySelectorAll('input');
+			[...appInputs].map((input) => {
+				input.addEventListener('onchange', this.renderLegend.bind(this));
+			});
+		},
+		init() {
+			this.bindEvents();
+		}
+	};
+
+	App.init();
+
 })();
